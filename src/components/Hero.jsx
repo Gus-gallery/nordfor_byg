55220cbc49fac17517a2d7fc19a94f9dd18f4ccc
Goose <207717976+Gus-gallery@users.noreply.github.com>
2025-11-12 15:38:10 +0100

    Hero opacity change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <section id="hero" className="relative flex items-center justify-center h-[70vh] overflow-hidden">
-        <img src="hero2.png" alt="Hero bg" className="absolute bottom-0 left-0 w-full h-auto min-h-full object-cover object-bottom opacity-50"/>
+        <img src="hero2.png" alt="Hero bg" className="absolute bottom-0 left-0 w-full h-auto min-h-full object-cover object-bottom opacity-70"/>
         
         <div className="relative z-10 text-center">
             <h1 className="text-5xl text-stone-800 font-bold text-center">
